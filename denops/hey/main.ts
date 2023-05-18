db6ffd508461d50e76c2f9b30244db020ac981ac
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-05-19 04:46:16 +0900

    format by prettier, fix type errors

--- denops/hey/main.ts
+++ denops/hey/main.ts
@@ -1,12 +1,15 @@
 import { ChatOpenAI } from "https://esm.sh/langchain@0.0.68/chat_models/openai";
-import { HumanChatMessage, SystemChatMessage } from "https://esm.sh/langchain@0.0.68/schema";
+import {
+  HumanChatMessage,
+  SystemChatMessage,
+} from "https://esm.sh/langchain@0.0.68/schema";
 import { Mutex } from "https://esm.sh/async-mutex@0.4.0";
 
 import { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
-import outdent from 'https://deno.land/x/outdent@v0.8.0/mod.ts';
+import outdent from "https://deno.land/x/outdent@v0.8.0/mod.ts";
 
 /**
  * The `hey` function sends a message to the ChatOpenAI model registered with Denops.
@@ -18,13 +21,23 @@ import outdent from 'https://deno.land/x/outdent@v0.8.0/mod.ts';
  * @param {AbortController} controller - The AbortController to abort the request
  * @returns {Promise<void>}
  */
-async function hey(denops: Denops, firstline: number, lastline: number, request: string, controller: AbortController) {
-  const precontext = (await fn.getline(denops, Math.max(firstline - 20, 0), firstline - 1)).join("\n");
-  const postcontext = (await fn.getline(denops, lastline + 1, lastline + 20)).join("\n");
+async function hey(
+  denops: Denops,
+  firstline: number,
+  lastline: number,
+  request: string,
+  controller: AbortController
+): Promise<void> {
+  const precontext = (
+    await fn.getline(denops, Math.max(firstline - 20, 0), firstline - 1)
+  ).join("\n");
+  const postcontext = (
+    await fn.getline(denops, lastline + 1, lastline + 20)
+  ).join("\n");
   const context = (await fn.getline(denops, firstline, lastline)).join("\n");
-  const indent = " ".repeat(await fn.indent(denops, firstline));
+  const indent = " ".repeat((await fn.indent(denops, firstline)) as number);
   const mutex = new Mutex();
-  await fn.deletebufline(denops, "%", firstline+1, lastline);
+  await fn.deletebufline(denops, "%", firstline + 1, lastline);
   await fn.setline(denops, firstline, [indent]);
   await fn.setcursorcharpos(denops, firstline, 0);
 
@@ -38,20 +51,23 @@ async function hey(denops: Denops, firstline: number, lastline: number, request:
           await mutex.runExclusive(async () => {
             const crow = await fn.line(denops, ".");
             const cline = await fn.getline(denops, crow);
-            const lines = (cline + token).replace("\n", "\n"+indent).split("\n");
+            const lines = (cline + token)
+              .replace("\n", "\n" + indent)
+              .split("\n");
             const nrow = crow + lines.length - 1;
-            const ncol = Array.from(new Intl.Segmenter().segment(lines.at(-1))).length;
+            const ncol = Array.from(
+              new Intl.Segmenter().segment(lines.at(-1)!)
+            ).length;
             await fn.append(denops, crow, Array(lines.length - 1).fill(""));
             await fn.setline(denops, crow, lines);
             await fn.setcursorcharpos(denops, nrow, ncol);
           });
-        }
-      }
-    ]
+        },
+      },
+    ],
   });
-
   const systemPrompt = outdent`
-    Act a professional ${ await vars.o.get(denops, "filetype") } writer for:
+    Act a professional ${await vars.o.get(denops, "filetype")} writer for:
     - helping human to write code (e.g., auto-completion)
     - helping human to write prose (e.g., grammar/ spelling correction)
 
@@ -62,23 +78,23 @@ async function hey(denops: Denops, firstline: number, lastline: number, request:
     - Must generate the concise text for any input.
 
     The following is the example of the input.
-    <Prompt>${ request }</Prompt>
-    <PreContext>${ outdent.string("\n"+precontext) }</PreContext>
-    <Target>${ outdent.string("\n"+context) }</Target>
-    <PostContext>${ outdent.string("\n"+postcontext) }</PostContext>
+    <Prompt>${request}</Prompt>
+    <PreContext>${outdent.string("\n" + precontext)}</PreContext>
+    <Target>${outdent.string("\n" + context)}</Target>
+    <PostContext>${outdent.string("\n" + postcontext)}</PostContext>
   `;
 
   const userPrompt = outdent`
-    <Prompt>${ request }</Prompt>
-    <PreContext>${ outdent.string("\n"+precontext) }</PreContext>
-    <Target>${ outdent.string("\n"+context) }</Target>
-    <PostContext>${ outdent.string("\n"+postcontext) }</PostContext>
+    <Prompt>${request}</Prompt>
+    <PreContext>${outdent.string("\n" + precontext)}</PreContext>
+    <Target>${outdent.string("\n" + context)}</Target>
+    <PostContext>${outdent.string("\n" + postcontext)}</PostContext>
   `;
 
-  await model.call([
-    new SystemChatMessage(systemPrompt),
-    new HumanChatMessage(userPrompt)
-  ], { options: { signal: controller.signal }});
+  await model.call(
+    [new SystemChatMessage(systemPrompt), new HumanChatMessage(userPrompt)],
+    { options: { signal: controller.signal } }
+  );
 }
 
 export async function main(denops: Denops) {
@@ -86,11 +102,15 @@ export async function main(denops: Denops) {
   const seq_curs: number[] = [];
   let myfirstline = 0;
   let mylastline = 0;
-  let myprompt = ""
+  let myprompt = "";
 
   denops.dispatcher = {
-    async hey(afistline: number, alastline: number, aprompt: string) {
-      seq_curs.push((await fn.undotree(denops)).seq_cur);
+    async hey(...args) {
+      const afistline = args[0] as number;
+      const alastline = args[1] as number;
+      const aprompt = args[2] as string;
+      const { seq_cur } = (await fn.undotree(denops)) as { seq_cur: number };
+      seq_curs.push(seq_cur);
       myfirstline = afistline;
       mylastline = alastline;
       myprompt = aprompt;
@@ -107,7 +127,8 @@ export async function main(denops: Denops) {
       await denops.cmd(`undo ${seq_curs.pop()}`);
     },
     async again() {
-      seq_curs.push((await fn.undotree(denops)).seq_cur);
+      const { seq_cur } = (await fn.undotree(denops)) as { seq_cur: number };
+      seq_curs.push(seq_cur);
       await denops.cmd(`undo ${seq_curs.at(-2)}`);
       try {
         controller = new AbortController();
@@ -118,11 +139,15 @@ export async function main(denops: Denops) {
         controller = undefined;
       }
     },
-    abort() {
-      controller?.abort();
-    }
+    abort: () =>
+      new Promise((res) => {
+        controller?.abort();
+        res(null);
+      }),
   };
-  await helper.execute(denops, outdent`
+  await helper.execute(
+    denops,
+    outdent`
     function! Hey(prompt) range abort
       call denops#notify("${denops.name}", "hey", [a:firstline, a:lastline, a:prompt])
     endfunction
@@ -145,5 +170,6 @@ export async function main(denops: Denops) {
     endfunction
     command! HeyAgain call HeyAgain()
     map <Plug>HeyAgain <Cmd>HeyAgain<CR>
-  `)
+  `
+  );
 }
